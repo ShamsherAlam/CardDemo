90d74bc12785fa0e71e4cd9c73e3f6c25f3a5128
ShamsherAlam <same.ddps@gmail.com>
2020-04-27 12:55:00 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'card';
+  posts = [{
+    imageUrl : 'assets/1.jpeg',
+    title : 'Actress 1',
+    description : 'Model 1'
+  },
+  {
+    imageUrl : 'assets/2.jpeg',
+    title : 'Actress 2',
+    description : 'Model 2'
+  },{
+    imageUrl : 'assets/3.jpeg',
+    title : 'Actress 3',
+    description : 'Model 3'
+  },{
+    imageUrl : 'assets/4.jpeg',
+    title : 'Actress 4',
+    description : 'Model 4'
+  },{
+    imageUrl : 'assets/5.jpeg',
+    title : 'Actress 5',
+    description : 'Model 5'
+  },{
+    imageUrl : 'assets/6.jpeg',
+    title : 'Actress 6',
+    description : 'Model 6'
+  }
+];
 }
